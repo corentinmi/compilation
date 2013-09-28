005fbc4ffef3dc1a3719f9b6b1b159f139b9f24e
ploutch <ploutch>
2013-09-28 18:23:35 +0200

    première regex, on peut checker une variable

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -1,3 +1,9 @@
+import identifiers.Variable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class LexicalAnalyzer {
 
@@ -6,7 +12,20 @@ public class LexicalAnalyzer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String line = new String();
+		System.out.println("Bonjour");
+		
+		while(true) {
+			try{
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				while((line=br.readLine())!=null){
+					Variable.check(line);
+				}
+		 
+			}catch(IOException io){
+				io.printStackTrace();
+			}	
+		}
 	}
 
 }
